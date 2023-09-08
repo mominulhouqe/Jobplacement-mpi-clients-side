d6a72bf762279f6632208ecd7d7e843a178a8609
mominul <ulhouqemomin39@gmail.com>
2023-09-08 20:57:51 +0600

    updated todoItems

--- src/pages/Home/Task.jsx
+++ src/pages/Home/Task.jsx
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import TodoList from "./TodoList";
 
 const Task = ({ addTodo }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   const handleAddTodo = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/todos', {
+      if (!text.trim()) {
+        // Check if the input is empty or only contains whitespace
+        return; // Don't add an empty todo
+      }
+
+      const response = await axios.post("http://localhost:5000/api/todos", {
         text,
       });
       addTodo(response.data); // Update the UI with the newly created Todo
-      setText('');
+      setText("");
     } catch (error) {
-      console.error('Error adding Todo:', error);
+      console.error("Error adding Todo:", error);
     }
   };
 
   return (
-    <div className="flex items-center space-x-2">
-      <input
-        type="text"
-        className="border border-gray-300 rounded px-2 py-1 w-full"
-        placeholder="Add a new Todo..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button
-        className="bg-blue-500 text-white px-3 py-1 rounded"
-        onClick={handleAddTodo}
-      >
-        Add Todo
-      </button>
-    </div>
+    <div>
+        <div className="flex  items-center space-x-2">
+          <input
+            type="text"
+            className="border border-gray-300 rounded px-2 py-1 w-full"
+            placeholder="Add a new Todo..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button
+            className={`bg-blue-500 text-white px-3 py-1 rounded ${
+              !text.trim() ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            onClick={handleAddTodo}
+            disabled={!text.trim()} // Disable the button when the input is empty or contains only whitespace
+          >
+            Add Todo
+          </button>
+        </div>
+      
+      </div>
+  
   );
 };
 
--- src/pages/Home/TodoItem.jsx
+++ src/pages/Home/TodoItem.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import { FaTrash } from 'react-icons/fa';
+import { BsPencil } from 'react-icons/bs';
 
 const TodoItem = ({ todo, onDelete }) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -7,35 +10,56 @@ const TodoItem = ({ todo, onDelete }) => {
 
   const handleUpdateTodo = async () => {
     try {
-      await axios.put(`http://localhost:5000/api/todos/${todo._id}`, {
+      await axios.put(`http://localhost:5000/api/todos/${todo._id}?_=${Math.random()}`, {
         text,
         completed: todo.completed,
       });
+
       setIsEditing(false);
+      Swal.fire({
+        icon: 'success',
+        title: 'Todo Updated',
+        text: 'Your todo has been updated successfully!',
+      });
     } catch (error) {
       console.error('Error updating Todo:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'An error occurred while updating the todo.',
+      });
     }
   };
 
   const handleDeleteTodo = async () => {
     try {
       await axios.delete(`http://localhost:5000/api/todos/${todo._id}`);
-      onDelete(todo._id); // Remove the deleted Todo from the UI
+      onDelete(todo._id);
+      Swal.fire({
+        icon: 'success',
+        title: 'Todo Deleted',
+        text: 'Your todo has been deleted successfully!',
+      });
     } catch (error) {
       console.error('Error deleting Todo:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'An error occurred while deleting the todo.',
+      });
     }
   };
 
   return (
-    <li className="flex items-center space-x-2">
+    <li className={`flex border rounded-md p-2 bg-slate-200 mt-2 mb-2 items-center space-x-2 ${isEditing ? 'bg-gray-100' : ''}`}>
       {isEditing ? (
         <>
-          <input
-            type="text"
-            className="border border-gray-300 rounded px-2 py-1"
+          <textarea
+            rows="3" // You can adjust the number of rows as needed
+            className="border border-gray-300 rounded px-2 py-1 resize-none"
             value={text}
             onChange={(e) => setText(e.target.value)}
-          />
+          ></textarea>
           <button
             className="bg-blue-500 text-white px-3 py-1 rounded"
             onClick={handleUpdateTodo}
@@ -50,9 +74,7 @@ const TodoItem = ({ todo, onDelete }) => {
             className="form-checkbox text-blue-500 h-5 w-5"
           />
           <span
-            className={`${
-              todo.completed ? 'line-through text-gray-400' : ''
-            }`}
+            className={`text-lg ${todo.completed ? 'line-through text-gray-400' : ''}`}
           >
             {todo.text}
           </span>
@@ -60,13 +82,13 @@ const TodoItem = ({ todo, onDelete }) => {
             className="text-blue-500 hover:underline"
             onClick={() => setIsEditing(true)}
           >
-            Edit
+            <BsPencil /> Edit
           </button>
           <button
             className="text-red-500 hover:underline"
             onClick={handleDeleteTodo}
           >
-            Delete
+            <FaTrash /> Delete
           </button>
         </>
       )}
