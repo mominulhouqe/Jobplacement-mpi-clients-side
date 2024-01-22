61d8edc889299409652bbf17a3b91e284a1366cc
mominul <ulhouqemomin39@gmail.com>
2024-01-22 18:28:19 +0600

    updated navber

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -14,7 +14,6 @@ import {
 } from "@mui/material";
 import { FaSignOutAlt, FaBars, FaTimes, FaUser } from "react-icons/fa";
 import { AuthContext } from "../../provider/AuthProvider";
-import "./Navbar.css";
 import logo from "../../assets/download.jpg";
 
 const Navbar = () => {
@@ -41,8 +40,8 @@ const Navbar = () => {
   };
 
   const handleUserProfileClick = () => {
-    navigate("/userprofile"); // Replace with the actual route for the user profile
-    handleProfileClose(); // Close the dropdown menu after navigation
+    navigate("/userprofile");
+    handleProfileClose();
   };
 
   const handleItemClick = () => {
@@ -50,144 +49,146 @@ const Navbar = () => {
   };
 
   return (
-    <div>
-      <AppBar className="fixed top-0 z-10"  color="primary">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    <AppBar color="primary">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link
+            to="/"
+            onClick={handleItemClick}
+            style={{
+              textDecoration: "none",
+              color: "#FFF",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <img src={logo} alt="" className="w-10 border rounded-full" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="auto"
+              height="40"
+              version="1.1"
+            >
+              <text
+                x="10"
+                y="30"
+                fontFamily="Arial"
+                fontSize="18"
+                fill="#ffffff"
+              >
+                {" "}
+                JobPlacement ~ MBPI
+              </text>
+            </svg>
+          </Link>
+        </Typography>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={() => setOpen(!open)}
+          sx={{ display: { md: "none" } }}
+        >
+          {open ? <FaTimes /> : <FaBars />}
+        </IconButton>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-11 " : "top-[-490px]"
+          }`}
+        >
+          <li className="md:ml-4 my-2">
+            <Link
+              to="/forms"
+              className="text-white px-2 py-1 rounded-lg duration-500"
+              onClick={handleItemClick}
+            >
+              FORMS
+            </Link>
+          </li>
+          <li className="md:ml-4 my-2">
+            <Link
+              to="/gallary"
+              className="text-white px-2 py-1 rounded-lg duration-500"
+              onClick={handleItemClick}
+            >
+              GALLERY
+            </Link>
+          </li>
+          <li className="md:ml-4 my-2">
             <Link
               to="/"
+              className="text-white px-2 py-1 rounded-lg duration-500"
               onClick={handleItemClick}
-              style={{ textDecoration: "none", color: "#FFF" }}
-              className="flex justify-center items-center"
             >
-              <img src={logo} alt="" className="w-10 border rounded-full" />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="auto"
-                height="40"
-                version="1.1"
-              >
-                <text
-                  x="10"
-                  y="30"
-                  fontFamily="Arial"
-                  fontSize="24"
-                  fill="#ffffff"
-                >
-                  {" "}
-                  JobPlacement ~ MBPI
-                </text>
-              </svg>
+              CONTACT
             </Link>
-          </Typography>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={() => setOpen(!open)}
-            sx={{ display: { md: "none" } }}
-          >
-            {open ? <FaTimes /> : <FaBars />}
-          </IconButton>
-          <ul
-            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-primary md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-              open ? "top-11 " : "top-[-490px]"
-            }`}
-          >
-            <li className="md:ml-4 my-2">
+          </li>
+          {user ? (
+            <li role="none" className="flex items-stretch">
               <Link
-                to="/forms"
-                className="text-white px-2 py-1 rounded-lg duration-500"
+                to="/dashboard/userhome"
+                role="menuitem"
+                className="flex items-center gap-2 py-4  uppercase transition-colors duration-300 hover:text-[#fe0800] focus:text-[#fe0800] focus:outline-none  lg:px-8"
+                href="#"
                 onClick={handleItemClick}
               >
-                FORMS
+                <span>Dashboard</span>
               </Link>
             </li>
-            <li className="md:ml-4 my-2">
-              <Link
-                to="/gallary"
-                className="text-white px-2 py-1 rounded-lg duration-500"
-                onClick={handleItemClick}
+          ) : null}
+        </ul>
+        <div className="md:ml-4">
+          {user ? (
+            <div>
+              <IconButton
+                color="inherit"
+                onClick={handleProfileClick}
+                sx={{ padding: 0 }}
               >
-                GALLERY
-              </Link>
-            </li>
-            <li className="md:ml-4 my-2">
-              <Link
-                to="/"
-                className="text-white px-2 py-1 rounded-lg duration-500"
-                onClick={handleItemClick}
+                {user.photoURL ? (
+                  <img
+                    src={user.photoURL}
+                    alt={user.displayName}
+                    className="w-10 h-10 rounded-full mr-2"
+                  />
+                ) : (
+                  <FaUser fontSize="large" />
+                )}
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleProfileClose}
+                PaperProps={{
+                  component: Paper,
+                  sx: { padding: 2 },
+                }}
               >
-                CONTACT
-              </Link>
-            </li>
-            {user ? (
-              <li role="none" className="flex items-stretch">
-                <Link
-                  to="/dashboard/userhome"
-                  role="menuitem"
-                  className="flex items-center gap-2 py-4  uppercase transition-colors duration-300 hover:text-[#fe0800] focus:text-[#fe0800] focus:outline-none  lg:px-8"
-                  href="#"
-                  onClick={handleItemClick}
-                >
-                  <span>Dashboard</span>
-                </Link>
-              </li>
-            ) : null}
-          </ul>
-          <div className="md:ml-4">
-            {user ? (
-              <div>
-                <IconButton
-                  color="inherit"
-                  onClick={handleProfileClick}
-                  sx={{ padding: 0 }}
-                >
-                  {user.photoURL ? (
-                    <img
-                      src={user.photoURL}
-                      alt={user.displayName}
-                      className="w-10 h-10 rounded-full mr-2"
-                    />
-                  ) : (
-                    <FaUser fontSize="large" />
-                  )}
-                </IconButton>
-                <Menu
-                  anchorEl={anchorEl}
-                  open={Boolean(anchorEl)}
-                  onClose={handleProfileClose}
-                  PaperProps={{
-                    component: Paper,
-                    sx: { padding: 2 },
-                  }}
-                >
-                  <MenuItem onClick={handleUserProfileClick}>
-                    <ListItemText primary="Profile" />
-                  </MenuItem>
-                  <MenuItem onClick={handleLogout}>
-                    <ListItemIcon>
-                      <FaSignOutAlt fontSize="small" />
-                    </ListItemIcon>
-                    <ListItemText primary="Logout" />
-                  </MenuItem>
-                </Menu>
-              </div>
-            ) : (
-              <Link to="/login" style={{ textDecoration: "none" }}>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  sx={{ textTransform: "none" }}
-                >
-                  Sign In
-                </Button>
-              </Link>
-            )}
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+                <MenuItem onClick={handleUserProfileClick}>
+                  <ListItemText primary="Profile" />
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  <ListItemIcon>
+                    <FaSignOutAlt fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Logout" />
+                </MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <Button
+                variant="contained"
+                color="secondary"
+                sx={{ textTransform: "none" }}
+              >
+                Sign In
+              </Button>
+            </Link>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
