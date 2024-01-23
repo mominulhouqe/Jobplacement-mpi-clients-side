9086f1b7b1c9994a34f6bf4be66ff4230541b5e4
mominul <ulhouqemomin39@gmail.com>
2024-01-23 13:45:36 +0600

    updated userinfo

--- src/pages/Users/UserInfo.jsx
+++ src/pages/Users/UserInfo.jsx
@@ -1,16 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
-import {
-  FaUser,
-  FaPhone,
-  FaBusinessTime,
-  FaWallet,
-  FaWindowClose,
-} from "react-icons/fa";
-
+import img2 from "../../assets/background-imge.png";
 const UserInfo = () => {
   const { user, loading } = useContext(AuthContext);
+  const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `https://userinformation.vercel.app/users?email=${user.email}`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+
+    if (user && user.email) {
+      fetchUserData();
+    }
+  }, [user]);
   if (loading) {
     return (
       <div className="flex items-center justify-center mt-24">
@@ -19,44 +35,35 @@ const UserInfo = () => {
     );
   }
   return (
-    <div className="p-6 my-12 mt-16 flex justify-center items-center">
-      <div className="bg-gray-100 shadow-md p-6 rounded-md">
-        <div className="flex justify-center items-center ">
-          <img className="rounded-full w-28 h-28" src={user.photoURL} alt="" />
-          {/* <FaUser className="text-4xl text-blue-500" /> */}
-        </div>
-        <div className="flex justify-center items-center mb-4">
-          <div>
-            <h1 className="text-3xl font-bold">{user.displayName}</h1>
-            <p className="text-gray-500">{user.designation}</p>
+    <div className="p-6 my-14 flex justify-center items-center">
+      {userData && (
+        <div className="border rounded-lg bg-slate-50 text-xs">
+          <div className="text-center mb-4">
+            <img
+              src={img2}
+              alt=""
+              className="w-full h-24 object-cover opacity-80"
+            />
+            <img
+              className="rounded-full w-20 h-20 object-fill mx-auto -translate-y-1/2 border"
+              src={user?.photoURL || "default-photo-url.jpg"}
+              alt=""
+            />
+            <h4 className="mb-2 font-medium text-lg">{user?.displayName}</h4>
+            <div className="text-sm text-gray-600"></div>
+          </div>
+          <div className="border-b border-gray-400 my-4"></div>
+          <div className="p-2  whitespace-nowrap">
+            <p className="p-2 border-b">
+              <span className="font-medium ">Role:</span>{" "}
+              {user?.role || "No role available"}
+            </p>
+            <p className="p-2 border-b">
+              <span className="font-medium">Email:</span> {user?.email}
+            </p>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="flex items-center">
-            <FaUser className="text-2xl text-blue-500" />
-            <span className="ml-2 font-semibold">{user.gender}</span>
-          </div>
-          <div className="flex items-center">
-            <span className="ml-2">{user.email}</span>
-          </div>
-          <div className="flex items-center">
-            <FaPhone className="text-2xl text-purple-500" />
-            <span className="ml-2">{user.phoneNumber}</span>
-          </div>
-          <div className="flex items-center">
-            <FaBusinessTime className="text-2xl text-orange-500" />
-            <span className="ml-2">{user.time}</span>
-          </div>
-          <div className="flex items-center">
-            <FaWallet className="text-2xl text-indigo-500" />
-            <span className="ml-2 font-semibold">{user.designation}</span>
-          </div>
-          <div className="flex items-center">
-            <FaWindowClose className="text-2xl text-red-500" />
-            <span className="ml-2">{user.gender}</span>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
