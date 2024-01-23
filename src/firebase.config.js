48f825d00d3350502a0036be2799eb2469640454
mominul <ulhouqemomin39@gmail.com>
2024-01-23 13:57:28 +0600

    udpated firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,22 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId,
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export default app;
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -24,14 +5,33 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcFT74Q_oVqPsBDxFKrhwRRZm2IrNleIw",
-  authDomain: "taskproject-b7e40.firebaseapp.com",
-  projectId: "taskproject-b7e40",
-  storageBucket: "taskproject-b7e40.appspot.com",
-  messagingSenderId: "38104050126",
-  appId: "1:38104050126:web:e3e2fb44be48376e41a39f"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+export default app;
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcFT74Q_oVqPsBDxFKrhwRRZm2IrNleIw",
+//   authDomain: "taskproject-b7e40.firebaseapp.com",
+//   projectId: "taskproject-b7e40",
+//   storageBucket: "taskproject-b7e40.appspot.com",
+//   messagingSenderId: "38104050126",
+//   appId: "1:38104050126:web:e3e2fb44be48376e41a39f"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app
